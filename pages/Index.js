18e9eeb8d1f90e3ce258ce3b516c18c1c204482f
lyarbrough-lab <leslie.yarbrough@gmail.com>
2025-07-08 20:39:01 -0500

    Create Index.js

---
+++ pages/Index.js
@@ -0,0 +1,10 @@
+import WatchtowerWithTikTok from '../components/WatchtowerWithTikTok';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>🛡️ Watchtower Dashboard</h1>
+      <WatchtowerWithTikTok />
+    </div>
+  );
+}
